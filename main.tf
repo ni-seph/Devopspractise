b9aa7b2e828626a054a1b0248300667483f21fa0
Niseph Shakya <niseph.shakya@gmail.com>
2020-10-22 13:57:07 -0500

    created load balancer and target group

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
+//instance
 provider "aws" {
     region = "us-east-2"
-    
 }
 resource "aws_instance" "myapp" {
     instance_type = var.instance_type
@@ -12,4 +12,34 @@ resource "aws_instance" "myapp" {
     timetolive = "10"
     backup = "no"
   }
+}
+// loadbalancer group
+resource "aws_lb" "test" {
+  name               = "testlb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["sg-0a9bcbe6cdbf27aca"]
+  subnets            = ["subnet-0037c83b82c89d773", "subnet-0473c8204338c09bd"]
+
+  enable_deletion_protection = false
+
+
+  tags = {
+    Environment = "devlopment"
+  }
+}
+//target group
+resource "aws_lb_target_group" "test" {
+  name     = "test-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id      = aws_vpc.main.id
+}
+resource "aws_vpc" "main" {
+  cidr_block = "172.31.16.0/20"
+}
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.myapp.id
+  port             = 80
 }
